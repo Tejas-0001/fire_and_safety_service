669cb2562c2317eceefb1ed4d86c4ce212ba646f
Tejas-0001 <202111080@diu.iiitvadodara.ac.in>
2024-02-28 22:33:04 +0530

    test commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Hero from "../components/hero/hero";
+import Contact from "../components/contact/contact";
+import Product from "../components/product/product";
+import Client from "../components/client/client";
+import Review from "../components/review/review";
+import Faq from "../components/faq/page";
+import Services from "../components/Services";
+
+export default function Home() {
+  return (
+    <main className="w-full h-full overflow-x-hidden flex flex-col content-center align-middle">
+      <img
+        className="-z-10 relative"
+        src="/hero.svg"
+        width={1920}
+        height={600}
+        alt="image"
+      ></img>
+      <Hero />
+      <Product />
+      <Client />
+      <Services />
+      <Review />
+      <Faq />
+      <Contact />
+    </main>
+  );
+}
